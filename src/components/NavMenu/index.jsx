267b622b422e7370779e56bca1e2d576dcc8b6f6
Jeff Anderson-Lester <viperhq@hotmail.com>
2025-03-24 01:12:24 -0500

    Added expandable navbar properties.

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -7,16 +7,19 @@ import logo from '../../assets/images/logo.gif'
 function NavMenu() {
     return (
         <>
-            <Navbar bg="dark" data-bs-theme="dark">
+            <Navbar bg="dark" data-bs-theme="dark" collapseOnSelect expand="lg">
                 <Container fluid>
                     <Navbar.Brand href="#home"><Image src={logo} /></Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="/garage">Garage</Nav.Link>
-                        <Nav.Link as={NavLink} to="/youtube">Videos</Nav.Link>
-                        {/* <Nav.Link as={NavLink} to="/blogs">Blogs</Nav.Link> */}
-                        <Nav.Link as={NavLink} to="/gallery">Galleries</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                            <Nav.Link as={NavLink} to="/garage">Garage</Nav.Link>
+                            <Nav.Link as={NavLink} to="/youtube">Videos</Nav.Link>
+                            {/* <Nav.Link as={NavLink} to="/blogs">Blogs</Nav.Link> */}
+                            <Nav.Link as={NavLink} to="/gallery">Galleries</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
