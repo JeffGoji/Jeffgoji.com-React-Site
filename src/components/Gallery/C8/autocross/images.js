bf66809e2250c2e9deaa0c12a4584e2fc66a36f1
JeffGoji <jeffgoji@gmail.com>
2025-03-27 20:10:28 -0500

    Added more images to C8 gallery and renamed files.

--- src/components/Gallery/C8/autocross/images.js
+++ src/components/Gallery/C8/autocross/images.js
@@ -1,6 +1,15 @@
 import c8ax001 from '../../../../assets/images/c8/autocross/20240316_151418.jpg';
 import c8ax002 from '../../../../assets/images/c8/autocross/20240317_092336.jpg';
-
+import c8ax003 from '../../../../assets/images/c8/autocross/1726961265575.jpg';
+import c8ax004 from '../../../../assets/images/c8/autocross/1726961271483.jpg';
+import c8ax005 from '../../../../assets/images/c8/autocross/1726961280569.jpg';
+import c8ax006 from '../../../../assets/images/c8/autocross/1742765180780.jpg';
+import c8ax007 from '../../../../assets/images/c8/autocross/1742765205464.jpg';
+import c8ax008 from '../../../../assets/images/c8/autocross/1742765215240.jpg';
+import c8ax009 from '../../../../assets/images/c8/autocross/1742765225015.jpg';
+import c8ax010 from '../../../../assets/images/c8/autocross/1742765232407.jpg';
+import c8ax011 from '../../../../assets/images/c8/autocross/BMWCCAAutoX Feb-4057.jpg';
+import c8ax012 from '../../../../assets/images/c8/autocross/BMWCCAAutoX Feb-4163.jpg';
 
 import "react-image-gallery/styles/css/image-gallery.css";
 
@@ -18,9 +27,91 @@ const images = [
         thumbnail: c8ax002,
         alt: 'First autocross with the C8',
         loading: 'lazy',
-        originalAlt: "First autocross with the C8",
-        description: "First autocross with the C8"
-    }
+        originalAlt: "First autocross with the C8, you can feel the excitement in the grid!",
+        description: "First autocross with the C8, you can feel the excitement in the grid!"
+    },
+    {
+        original: c8ax003,
+        thumbnail: c8ax003,
+        alt: 'Panda killing it on track',
+        loading: 'lazy',
+        originalAlt: "Panda killing it on track",
+        description: "Panda killing it on track"
+    },
+    {
+        original: c8ax004,
+        thumbnail: c8ax004,
+        alt: 'Cornering on the stock Michelin Pilot tires',
+        loading: 'lazy',
+        originalAlt: "Cornering on the stock Michelin Pilot tires",
+        description: "Cornering on the stock Michelin Pilot tires"
+    },
+    {
+        original: c8ax005,
+        thumbnail: c8ax005,
+        alt: 'Great side shot by David Leung',
+        loading: 'lazy',
+        originalAlt: "Great side shot by David Leung",
+        description: "Great side shot by David Leung"
+    },
+    {
+        original: c8ax006,
+        thumbnail: c8ax006,
+        alt: 'Micheal Legere in the C8',
+        loading: 'lazy',
+        originalAlt: "Micheal Legere in the C8",
+        description: "My co-driver for 2024, Micheal Legere, in the C8 with me riding shotgun"
+    },
+    {
+        original: c8ax007,
+        thumbnail: c8ax007,
+        alt: 'Flying solo',
+        loading: 'lazy',
+        originalAlt: "Flying solo",
+        description: "Flying solo"
+    },
+    {
+        original: c8ax008,
+        thumbnail: c8ax008,
+        alt: 'Another great side shot',
+        loading: 'lazy',
+        originalAlt: "Another great side shot",
+        description: "Another great side shot"
+    },
+    {
+        original: c8ax009,
+        thumbnail: c8ax009,
+        alt: `It's shocking how good the front-end is`,
+        loading: 'lazy',
+        originalAlt: `It's shocking how good the front-end is`,
+        description: `It's shocking how good the front-end is`
+    },
+    {
+        original: c8ax010,
+        thumbnail: c8ax010,
+        alt: 'On the gas hard and the nose lifting slightly',
+        loading: 'lazy',
+        originalAlt: "On the gas hard and the nose lifting slightly",
+        description: "On the gas hard and the nose lifting slightly"
+    },
+    {
+        original: c8ax011,
+        thumbnail: c8ax011,
+        alt: 'My co-driver and I autocrossing Panda for the first time',
+        loading: 'lazy',
+        originalAlt: "My co-driver and I autocrossing Panda for the first time",
+        description: "My co-driver and I autocrossing Panda for the first time"
+    },
+    {
+        original: c8ax012,
+        thumbnail: c8ax012,
+        alt: 'My first solo run in the car',
+        loading: 'lazy',
+        originalAlt: "My first solo run in the car",
+        description: "My first solo run in the car"
+    },
+
+
 ];
 
 export default images;
