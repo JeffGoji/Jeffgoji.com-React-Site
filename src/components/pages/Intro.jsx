aa1e83c307f7081fa6544916c42f13c87dfea8c9
JeffGoji <jeffgoji@gmail.com>
2025-03-31 15:19:44 -0500

    Changed .svg image and webpage title. Worked on intro page

--- src/components/pages/Intro.jsx
+++ src/components/pages/Intro.jsx
@@ -8,9 +8,11 @@ function Intro() {
                     <div className='row mt-5 justify-content-center'>
                         <div className='col-lg-12'>
 
-                            <h1 className="text-center text-outline fw-bold mt-5">Jeff Goji&apos;s Car Blog</h1>
+                            <h1 className="text-center fw-bold mt-5">The Goji Line — Driving. Tuning. Living.</h1>
                             <h3 className='fw-bold mt-3'>
-                                Hey there! I’m Jeff, a lifelong car enthusiast with a soft spot for Miatas and Autocross. Here you’ll find my thoughts on modifications, event recaps, and the joys (and headaches) of daily driving. Dive into The Garage or catch up on my latest autocross videos below!
+                                Hey there! I’m Jeff, a lifelong car enthusiast with a soft spot for Miatas and Autocross.
+                                <br/>Here you’ll find my thoughts on modifications, event recaps, and the joys (and headaches) of daily driving.
+                                <br/>Dive into The Garage or catch up on my latest autocross videos below!
                             </h3>
                         </div>
                     </div>
