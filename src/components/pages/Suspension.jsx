00467462a1d90c9a2f91a06ebe1953580ff48f55
JeffGoji <jeffgoji@gmail.com>
2025-05-14 20:59:22 -0500

    Initial Commit

---
+++ src/components/pages/Suspension.jsx
@@ -0,0 +1,19 @@
+import { Container, Row, Col } from "react-bootstrap";
+function Suspension() {
+  return (
+      <Container fluid>
+          <Row>
+              <Col lg={12} className="text-center">
+                  <h1 className="text-center">Suspension Tuning</h1>
+              </Col>
+          </Row>
+          <Row>   
+              <Col lg={12} className="text-center">
+                  <p></p>
+              </Col>
+</Row>
+    </Container>
+  );
+}
+
+export default Suspension;
