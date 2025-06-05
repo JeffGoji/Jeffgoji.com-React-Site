688911840ffe5f7d41857e947a60660a682575cc
JeffGoji <jeffgoji@gmail.com>
2025-06-04 23:48:10 -0500

    Removed no longer used firebase files and config.

--- src/components/blog/index.jsx
+++ src/components/blog/index.jsx
@@ -1,32 +1,5 @@
-// Blog.js
-import { useEffect, useState } from 'react';
-import { firestore } from '../../firebase-config.js';
 
 const Blog = () => {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const snapshot = await firestore.collection('posts').get();
-                const postsData = snapshot.docs.map(doc => ({
-                    id: doc.id,
-                    ...doc.data()
-                }));
-                setPosts(postsData);
-            } catch (error) {
-                console.error("Error fetching posts:", error);
-            }
-            setLoading(false);
-        };
-
-        fetchPosts();
-    }, []);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
 
     return (
         <div>
