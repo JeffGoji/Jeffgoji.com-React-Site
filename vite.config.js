4dcd41a84364fbba3a472795486f952c9c51aaa6
Jeff Anderson-Lester <viperhq@hotmail.com>
2025-06-15 17:10:04 -0500

    Updated Vite.config to downgrade JSOM to ES5.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
+  build: {
+    // transpile optional chaining, nullish coalescing, etc. down to ES2015 so the JSDOM can parse it correctly for netlify.
+    target: 'es2015'
+  },
   plugins: [
-    react(),
-    // …any other plugins you originally had
+    react()
   ],
-  base: '/',
+  base: '/'
 })
