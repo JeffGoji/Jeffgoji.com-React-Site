a2f9605411bfec7b1e90b227d5539ab3363796b2
JeffGoji <jeffgoji@gmail.com>
2025-11-01 18:58:16 -0500

    Created initial article for Goodbye C8, added new article.

---
+++ src/components/Articles/2025/GoodbyeC8.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Container, Row, Col } from "react-bootstrap";
+
+
+function GoodbyeC8() {
+    
+    return (
+        <Container fluid className="article-page">
+            <Row className="justify-content-center mt-5">
+                <h1>Goodbye, Panda</h1>
+                <Col>
+                   
+                    <p>Here at the end of a short but fun road. After three years and 16,000 miles, it is time to say goodbye to the C8 Corvette.
+                        <br />
+                    So as the car departs, I want give an end of life report on it and fill you in on the positives and negatives of owning the first mid-engine Corvette.
+                    </p>
+                    <p>First, the positives.
+                        <br />
+                        The C8 is a blast to drive in any situation and mode selected. The mid-engine layout gives it a handling characteristic that is unlike any previous Corvette, it stays planted and gets power down out of a corner in a brutally efficient manner as the torque piles on from the 6.2 liter LT2 V8.
+                        <br />
+                        Passing on the highway is also fairly effortless, and you'll find yourself going quite a bit faster than intended from time to time unless you're paying close attention to the speedometer.
+                        The V8 engine provides plenty of power, and the dual-clutch transmission shifts quickly and smoothly enough for most daily driving situations. in fact, the DCT in the C8 has to be one of the best dual-clutch transmissions I have ever driven, and is easily a gold standard for daily driving and track performance.
+                    </p>
+                    <p>
+                        The car is fairly large (very wide) and a bit heavy. This C8 has the Z51 performance package, which adds a few extra performance goodies, like an aero package, magnetic ride control, and better brakes.
+                        <br/>
+                        I am sure a hundred or more reviewers have said this, but the mag-ride is a game changer for combining ride comfort for daily driving duties and stiffening up for a more spirited canyon-carving experience in sport mode, or going full-stiff in track mode for the occasional track day or autocross event.
+                        <br />
+                        The suspension is very compliant in tour mode, soaking up bumps and rough pavement with ease, making it a surprisingly comfortable car for long drives despite its sports car nature.
+                        <br/>
+                        I found Sport Mode to be the best for canyon carving in the Texas Hill Country, as it still allowed some compliance for the rougher roads while keeping body roll in check. It also makes the transmission shift quickler and holds gears longer for better acceleration out of corners.
+                    <br/>
+                        Track mode is best reserved for the track or autocross, as the ride gets very stiff and unforgiving on even what is considered to be "smooth" pavement.
+                        <br />
+                        This is where the DCT transmission really shines, holding gears longer and downshifting more aggressively for maximum performance.
+                        <br />
+                        You could always swap to Manual mode for the transmission and use the paddle shifters, but I found the DCT to be so good that I rarely felt the need to intervene.
+                    </p>
+                    <p>Another positive is the design of the car. The C8 has a sleek and modern look that turns heads wherever it goes. It also invites lots of questions and random conversations from various people about the car whenever it's parked in public spots.
+                    </p>
+                    <p>The interior of the first generation C8 (2020 to 2024) is a winner for me. Everything about it is functional and is focused like  cockpit in a fighter jet for the driver. There is the infamous "wall of buttons" between the seats which can lead to a tiny amount of passenger discomfort, but it makes AC and ventilated/heated seat control very easy to access for the passenger.
+                        This car came with the excellent race seats, and they were awesome for track and autocross but could be tiring on a long drive due to the high bolsters on the thighs.
+                        <br />
+                        We swapped those out for the mid-range GT2 seats which are more comfortable for daily driving and long trips, and they are still very supportive in the corners.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default GoodbyeC8;
