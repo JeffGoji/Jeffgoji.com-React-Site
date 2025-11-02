06fccc7fc9a3de145c6f86aea5cfa2af0f707aaf
JeffGoji <jeffgoji@gmail.com>
2025-11-01 20:42:12 -0500

    Settled on formatting for the article. Added new pictures to the article.

--- src/components/Articles/2025/GoodbyeC8.jsx
+++ src/components/Articles/2025/GoodbyeC8.jsx
@@ -1,30 +1,40 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Container, Row, Col } from "react-bootstrap";
-
-
+import c81 from '../../../assets/images/c8/c8-001s.jpg';
+import c82 from '../../../assets/images/c8/autocross/c8s-002.jpg';   
+import c83 from '../../../assets/images/c8/club-001.jpg';
+import c84 from '../../../assets/images/c8/autocross/c84.jpg';
+import c85 from '../../../assets/images/c8/shoprally_03292025.jpg';
 function GoodbyeC8() {
     
     return (
-        <Container fluid className="article-page">
+        <Container  className="article-page">
             <Row className="justify-content-center mt-5 text-center">
                 <h1>Goodbye, Panda</h1>
                 <Col>
                     <p>Here at the end of a short but fun road. After three years and 16,000 miles, it is time to say goodbye to the C8 Corvette.
                         <br />
-                    So as the car departs, I want give an end of life report on it and fill you in on the positives and negatives of owning the first mid-engine Corvette.
+                        So as the car departs, I want give an end of life report on it and fill you in on the positives and negatives of owning the first mid-engine Corvette.
+                        <br />
+                        First, the positives...
                     </p>
                     </Col>
             </Row>
-            <Row className="justify-content-center mt-3">
-                <Col>
-                    <p>First, the positives.
-                        <br />
-                        The C8 is a blast to drive in any situation and mode selected. The mid-engine layout gives it a handling characteristic that is unlike any previous Corvette, it stays planted and gets power down out of a corner in a brutally efficient manner as the torque piles on from the 6.2 liter LT2 V8.
+            <Row className="justify-content-center text-center mt-3">
+                <Col lg={12} md={12} sm={12}>
+                    <img src={c81} alt="Panda" className="img-fluid rounded" />
+                    <p className="text-start mt-3">
+                        The C8 is a blast to drive in any situation and mode selected. The mid-engine layout gives it a handling characteristic that is unlike any previous Corvette, mt-0"it stays planted and gets power down out of a corner in a brutally efficient manner as the torque piles on from the 6.2 liter LT2 V8.
                         <br />
                         Passing on the highway is also fairly effortless, and you'll find yourself going quite a bit faster than intended from time to time unless you're paying close attention to the speedometer.
                         The V8 engine provides plenty of power, and the dual-clutch transmission shifts quickly and smoothly enough for most daily driving situations. in fact, the DCT in the C8 has to be one of the best dual-clutch transmissions I have ever driven, and is easily a gold standard for daily driving and track performance.
                     </p>
-                    <p>
+                </Col>
+            </Row>
+            <Row className="justify-content-center text-center mt-3">
+                <Col lg={12} md={12} sm={12}>
+                    <img src={c82} alt="Panda" className="img-fluid rounded" />
+                    <p className="text-start mt-3">
                         The car is fairly large (very wide) and a bit heavy. This C8 has the Z51 performance package, which adds a few extra performance goodies, like an aero package, magnetic ride control, and better brakes.
                         <br/>
                         I am sure a hundred or more reviewers have said this, but the mag-ride is a game changer for combining ride comfort for daily driving duties and stiffening up for a more spirited canyon-carving experience in sport mode, or going full-stiff in track mode for the occasional track day or autocross event.
@@ -39,12 +49,47 @@ function GoodbyeC8() {
                         <br />
                         You could always swap to Manual mode for the transmission and use the paddle shifters, but I found the DCT to be so good that I rarely felt the need to intervene.
                     </p>
-                    <p>Another positive is the design of the car. The C8 has a sleek and modern look that turns heads wherever it goes. It also invites lots of questions and random conversations from various people about the car whenever it's parked in public spots.
+                </Col>
+            </Row>
+            <Row className="justify-content-center mt-3 text-center">
+                <Col lg={12} md={12} sm={12} className="justify-content-center text-center mt-3">
+                    <img src={c84} alt="Panda" className="img-fluid rounded" />
+                    <p className="text-start mt-3">
+                        One of the coolest features of the Corvette is the PTM (Performance Traction Management) system, which allows you to select different driving modes that adjust the car's traction control, stability control, and other performance settings.
+                        <br />
+                        This system is especially useful for track days or autocross events, as it allows you to tailor the car's performance to your driving style and the conditions of the track, this is especially useful if you're not yet comfortable pushing the car to and above it's absolute limits.
+                        <br />
+                        The system intervenes in a couple of ways. First it has several levels of traction and stability control that it can apply allowing for more slip and wheelspin as you go up in the settings from wet, to dry, to sport, to track 1, to track 2, and finally completely off.
+                        <br />
+                        The second way it intervenes is it will tighten the driving line by applying the brakes to individual wheels to help rotate the car and keep it on the intended line. This also has several levels from aggressive to completely off.
+                        <br />
+                        When I was learning the car, the PTM system was absolutely invaluable in helping me build confidence and learn the car's limits without going over them. As I got more comfortable, I was able to dial back the intervention and eventually turn it off completely for maximum performance.
+                        <br/>I am now my absolute fastest in this car with the PTM completely off.
                     </p>
-                    <p>The interior of the first generation C8 (2020 to 2024) is a winner for me. Everything about it is functional and is focused like  cockpit in a fighter jet for the driver. There is the infamous "wall of buttons" between the seats which can lead to a tiny amount of passenger discomfort, but it makes AC and ventilated/heated seat control very easy to access for the passenger.
+                </Col>
+            </Row>
+            <Row className="justify-content-center text-center mt-3">
+                <Col lg={12} md={12} sm={12}>
+                    <img src={c83} alt="Panda" className="img-fluid rounded" />
+                    <p className="text-start mt-3">Another positive is the design of the car. The C8 has a sleek and modern look that turns heads wherever it goes. It also invites lots of questions and random conversations from various people about the car whenever it's parked in public spots.
+                    </p>
+                    <p className="text-start mt-3">The interior of the first generation C8 (2020 to 2024) is a winner for me. Everything about it is functional and is focused like  cockpit in a fighter jet for the driver. The viewing area out the front of the car is awesome and is somewhere around 40% improved over the previous generation.
+                        <br/>There is the infamous "wall of buttons" between the seats which can lead to a tiny amount of passenger discomfort, but it makes AC and ventilated/heated seat control very easy to access for the passenger.
                         This car came with the excellent race seats, and they were awesome for track and autocross but could be tiring on a long drive due to the high bolsters on the thighs.
                         <br />
                         We swapped those out for the mid-range GT2 seats which are more comfortable for daily driving and long trips, and they are still very supportive in the corners.
+                    </p>
+                </Col>
+            </Row>
+            <Row className="justify-content-center text-center mt-3">
+                <Col lg={12} md={12} sm={12} className="justify-content-center text-center mt-3">
+                    <img src={c85} alt="Panda" className="img-fluid rounded" />
+                    <p className="text-start mt-3">
+                        So Panda is a 70th Anniversary Edition C8 with the Hardtop Convertable package, which replaces the removable roof panel with a power retractable hardtop.
+                        <br />
+                        This is a great option for those who want the open-air experience of a convertible without having to deal with removing and storing a roof panel. It also operates seemlessly with the top going up or down in about 16 seconds and can be done at speeds up to 30 mph.
+                        <br />
+                        If you ever intend to use your C8 as a grand touring car or live in a state where you can enjoy open air driving often, I highly recommend the hardtop convertable option.
                     </p>
                 </Col>
             </Row>
